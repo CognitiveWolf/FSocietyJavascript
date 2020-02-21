6c1cf6897bf9de9489011e58e6d2324658dfee5f
Jimmy Basiluk <vasilukwolf@gmail.com>
2020-02-21 14:54:42 -0500

    XMLHTTPRequest hack jon

---
+++ XMLHttpRequest.js
@@ -0,0 +1,9 @@
+var req = new XMLHttpRequest();
+req.onreadystatechange = function(){
+  if(req.readyState == 4 && req.status== 200){
+    document.getElementById("result").innerHTML = req.responseText;
+    alert(req.responseText);
+  }
+}
+req.open("GET","/lab/webapp/jfp/14/email?name=john",true);
+req.send();
